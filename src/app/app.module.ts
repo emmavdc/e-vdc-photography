bf846fca19b89036ad47e63553abb47e1271165e
Emma VANDECASTEELE <etu40268@henallux.be>
2021-08-10 15:34:33 +0200

    add lottie web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,7 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { ModeComponent } from './mode/mode.component';
 
 @NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
     AppComponent,
     AboutComponent,
