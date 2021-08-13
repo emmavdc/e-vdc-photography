5c2b3678894718fe14e91613b2f74fcbfd93e225
Emma VANDECASTEELE <etu40268@henallux.be>
2021-08-13 14:47:47 +0200

    pwa feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AboutComponent } from './components/about/about.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { ModeComponent } from './mode/mode.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
@@ -22,6 +24,12 @@ import { ModeComponent } from './mode/mode.component';
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
